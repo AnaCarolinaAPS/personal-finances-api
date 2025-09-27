4955236339b1655d5730df678414bf98f346739f
Ana Carolina <anacarolinaaps@gmail.com>
2025-09-27 17:42:35 -0300

    feat(accounts): add model and migration for Accounts
    - New model Account;
    - New migration create_accounts_table;

---
+++ app/Models/Account.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'currency_id',
+        'is_active',
+    ];
+
+}
---
+++ database/migrations/2025_09_27_202757_create_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->foreignId('currency_id')
+                    ->constrained('currencies')
+                    ->restrictOnDelete(); // Caso tentar apagar a moeda relacionada existindo contas causa erro
+            $table->boolean('is_active')->default(true); // Se a moeda está habilitada no sistema
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
